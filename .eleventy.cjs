18643682a4b363a7e8a46dbf8882d5491f1c50d2
yanlong.wang <yanlong.wang@naiver.org>
2021-12-17 17:01:28 +0800

    refactor(docs): fit in gh-pages rules

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -2,16 +2,16 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPassthroughCopy('docs/docs.css');
-  eleventyConfig.addPassthroughCopy('docs/.nojekyll');
+  eleventyConfig.addPassthroughCopy('src/docs/docs.css');
+  eleventyConfig.addPassthroughCopy('src/docs/.nojekyll');
   eleventyConfig.addPassthroughCopy(
     'node_modules/@webcomponents/webcomponentsjs'
   );
   eleventyConfig.addPassthroughCopy('node_modules/lit/polyfill-support.js');
   return {
     dir: {
-      input: 'docs',
-      output: 'docs/_build',
+      input: 'src/docs',
+      output: 'docs/',
     },
     templateExtensionAliases: {
       '11ty.cjs': '11ty.js',
