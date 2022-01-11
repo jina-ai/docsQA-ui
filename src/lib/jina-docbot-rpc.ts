87e1ac33d253a2fc6e0ff6c23d59cb96920966bf
yanlong.wang <yanlong.wang@naiver.org>
2022-01-11 19:03:41 +0800

    feat(rpc): append `?` to query text

--- src/lib/jina-docbot-rpc.ts
+++ src/lib/jina-docbot-rpc.ts
@@ -17,10 +17,13 @@ export class JinaDocBotRPC extends HTTPService {
     }
 
     async askQuestion(text: string) {
+        // To improve the quality of DPR results, always have `?` at the end of query text https://github.com/jina-ai/docsQA-ui/issues/14
+        const mangledText = text.trim().concat('?').replace(/?+$/, '?');
+
         const result = await this.postJson<
             JinaServerEnvelope<DocumentArray> &
             { [k: string]: any; }
-        >('/search', { data: [{ text }] });
+        >('/search', { data: [{ text: mangledText }] });
 
         result.data = { ...result.data, answer: get(result.data, 'data.docs[0].matches[0]') as Document };
         return result;
