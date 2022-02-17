c1089f722139306e7dfb9d8210484322e80debfb
Yanlong Wang <yanlong.wang@naiver.org>
2022-02-17 17:12:12 +0800

    feat: show complete sentence as answer

--- src/qa-bot/plugins/protocol-status.ts
+++ src/qa-bot/plugins/protocol-status.ts
@@ -14,8 +14,37 @@ export function mangleAnswerByStatus(this: DocQAAnswer, qaPair: QAPair) {
 
         case DOCQA_ANSWER_STATUS.ANSWERED: {
             qaPair.useTemplate = ANSWER_RENDER_TEMPLATE.TEXT_WITH_LINK;
-            qaPair.answer.text = get(this.matches, '[0].text');
-            qaPair.answer.uri = get(this.matches, '[0].uri');
+            const match = get(this.matches, '[0]');
+
+            const paragraph: string = get(match, 'tags.paragraph');
+            const sentenceStart = get(match, 'tags.sentence_start');
+            const sentenceEnd = get(match, 'tags.sentence_end');
+            const spanStart = get(match, 'tags.span_start');
+            const spanEnd = get(match, 'tags.span_end');
+
+            let answerText = match.text;
+            if (Number.isInteger(spanStart) && Number.isInteger(spanEnd)) {
+                answerText = paragraph.slice(spanStart, spanEnd);
+            }
+
+            let sentence = '';
+            if (Number.isInteger(sentenceStart) && Number.isInteger(sentenceEnd)) {
+                sentence = paragraph.slice(sentenceStart, sentenceEnd);
+            }
+
+            qaPair.answer.text = get(match, 'text');
+            qaPair.answer.uri = get(match, 'uri');
+
+            const splitVec = sentence.split(answerText);
+
+            if (splitVec.length === 2) {
+                const [a, b] = splitVec;
+                qaPair.answer.tags = {
+                    sentence,
+                    answerVec: [a, answerText, b]
+                };
+            }
+
             break;
         }
 
