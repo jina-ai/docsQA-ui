22b82116013fac9226dae08bc28bfd8684da31aa
yanlong.wang <yanlong.wang@naiver.org>
2022-03-07 12:27:01 +0800

    fix(dist): dont remove empty attr

--- rollup.config.js
+++ rollup.config.js
@@ -31,6 +31,7 @@ export default {
     minifyHTML({
       options: {
         minifyOptions: {
+          removeEmptyAttributes: false,
           minifyCSS: {
             level: {
               1: {
